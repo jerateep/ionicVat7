9c772dccd5bcf5dbe21c5b684634e9faea11067d
jerateep Saelee <jerateep_@live.com>
2020-01-25 11:42:54 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  vatResult = 0;
 
-  constructor() {}
+  calculate(money){
+    let result = money * 107/100;
+    this.vatResult = result;
+    //alert(money);
+  }
 
 }
